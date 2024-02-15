1819612f3728b4a3970030d5b46319bfdb99efe1
ic3bug <ic3bug@gmail.com>
2024-02-15 18:51:12 +0300

    Update
    Couple of small changes

--- scenes/game.gd
+++ scenes/game.gd
@@ -11,33 +11,32 @@ const PORT : int = 1337
 var menu : Control = null
 var map : Node = null
 
-
 func _ready():
 	menu = preload("res://scenes/menu.tscn").instantiate()
 	main.add_child(menu)
 	
-	# if multiplayer.is_server():
 	multiplayer.peer_connected.connect(spawn_player)
 	multiplayer.peer_disconnected.connect(remove_player)
 
 func load_map():
-	# Free old stuff.
+	# Free previously loaded maps and menus
 	if map != null:
 		map.queue_free()
 	if menu != null:
 		menu.queue_free()
 	
-	# Spawn map.
+	# Spawn map
 	map = preload("res://scenes/map.tscn").instantiate()
 	main.add_child(map)
 	
-	# if multiplayer.is_server():
+	# Spawn player
 	spawn_player(multiplayer.get_unique_id())
 
 func spawn_player(id: int):
 	var player = preload("res://scenes/player.tscn").instantiate()
 	player.peer_id = id
 	players.add_child(player, true)
+	player.global_transform.origin = Vector3.UP
 
 func remove_player(id: int):
 	if not players.has_node(str(id)):
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody3D
 
+# Movement constants
 const GRAVITY = 30.0
 const SPEED = 15.0
 const JUMP_VELOCITY = 10.0
 
-# Peer id.
+# Mouse
+@export var mouse_sensitivity : float = 3.0
+
+# Peer identification
 @export var peer_id : int : 
 	set(value):
 		peer_id = value
@@ -13,29 +17,31 @@ const JUMP_VELOCITY = 10.0
 		set_multiplayer_authority(peer_id)
 
 func _ready():
-	# Set local camera.
+	# Capture the mouse on player spawn
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	# Set the camera as the current camera
 	$Camera3D.current = peer_id == multiplayer.get_unique_id()
-	# Set process functions for current player.
+	# Set process functions for current player
 	var is_local = is_multiplayer_authority()
 	set_process_input(is_local)
 	set_physics_process(is_local)
 	set_process(is_local)
 
 func _process(_delta):
-	# Handle mouse capture.
+	# Handle mouse capture
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
 
-	# Handle Jump.
+	# Handle Jump
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction and handle the movement
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -45,11 +51,15 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	# Godot's movement and collision detection function
 	move_and_slide()
 
 func _input(event):
+	# If mouse is visible do nothing
 	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		return
-	# Handle body rotation.
+	# Handle body rotation
 	if event is InputEventMouseMotion:
-		rotation.y += event.relative.x * -0.001
+		rotation.y -= event.relative.x * 0.001 * mouse_sensitivity
+		$Camera3D.rotation.x -= event.relative.y * 0.001 * mouse_sensitivity
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.5, 1.5)
